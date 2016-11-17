e8207311f1dc52de4a4751d21a300eb1e9769b7c
magno <magno@192.168.1.100>
2016-11-16 22:10:44 -0200

    construção do software 6.5% pronto

--- src/utfpr/edu/br/papelariafacil/UtfprEduBrPapelariaFacil.java
+++ src/utfpr/edu/br/papelariafacil/UtfprEduBrPapelariaFacil.java
@@ -21,7 +21,7 @@ public class UtfprEduBrPapelariaFacil {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-     
+         
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Windows".equals(info.getName())) {
@@ -32,9 +32,9 @@ public class UtfprEduBrPapelariaFacil {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(FrmBase.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        FrmAcessoLogin f = new FrmAcessoLogin();
-        Util.abrirJFrameCentralizado(f);
-        f.setVisible(true);
+        FrmAcessoLogin fr = new FrmAcessoLogin();
+        Util.abrirJFrameCentralizado(fr);
+        fr.setVisible(true);
      
     }
     
